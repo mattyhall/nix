f515d037d299c7cad54c7d4672de712355b493b9
Matt Hall <me@mattjhall.co.uk>
2023-11-01 08:21:31 +0000

    Add jinja to nvim config

--- home-manager/extra/nvim/init.lua
+++ home-manager/extra/nvim/init.lua
@@ -11,6 +11,8 @@ require('packer').startup(function(use)
   -- Package manager
   use 'wbthomason/packer.nvim'
 
+  use 'lepture/vim-jinja'
+
   use { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     requires = {
